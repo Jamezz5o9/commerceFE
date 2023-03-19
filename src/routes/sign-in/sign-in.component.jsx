f46d4bd962bb5d3b85ff31f64549c406bd4b8ce5
James <adulojujames@gmail.com>
2023-03-19 18:01:01 +0100

    firebase added

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
+
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const {user} = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user)
+}
+
   return (
     <div>
         <h1>Sign in page</h1>
+        <button onClick={logGoogleUser}>
+          Sign in with google pop up
+        </button>
     </div>
   )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import { initializeApp } from 'firebase/app'
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
 
 
 const firebaseConfig = {
@@ -10,8 +16,44 @@ const firebaseConfig = {
     messagingSenderId: "730969682414",
     appId: "1:730969682414:web:3e2be5c648e05fbed2deff"
   };
+
+
   
 
   const firebaseApp = initializeApp(firebaseConfig);
 
   const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+    prompt: "select_account"
+  })
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef)
+    const userSnapshot = await getDoc(userDocRef);     
+ 
+
+    if(!userSnapshot.exists()){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+          await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+        });
+
+      } catch(error){
+        console.log('error creating the user', error.message);
+      }
+
+    }
+    return userDocRef;
+
+  };
